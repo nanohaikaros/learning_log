f20f18fe235e12c945e8b201996e5efaaead369c
ikaros <1371689491@qq.com>
2017-10-27 09:46:32 +0800

    Initial commit

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\workspace\py3\python crash course\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
