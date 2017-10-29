aecc7e70a2a7b5d940bbcfc4f0e3e6d878d905eb
ikaros <1371689491@qq.com>
2017-10-29 13:53:07 +0800

    使用Bootstrap 来设置项目“学习笔记”的样式

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 第三方应用程序
+    'bootstrap3',
+
     # 我的应用程序
     'learning_logs',
     'users'
@@ -122,9 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# 第三方应用程序
-'bootstrap3'
 
 # 我的设置
 LOGIN_URL = '/users/login/'
